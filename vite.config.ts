565cd2fac2148ec0cf2fb4865178eb86727bf325
maxime <maximemoya@gmail.com>
2025-06-14 02:33:55 +0200

    remove base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pixi-project/",
+  //base: "/pixi-project/",
   build: {
     outDir: "docs",
   },
