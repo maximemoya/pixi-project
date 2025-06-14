69dc9e0193ec7018af8f99c3d6a099a4edfcec5f
maxime <maximemoya@gmail.com>
2025-06-14 02:29:20 +0200

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pixi-project/",
+  build: {
+    outDir: "docs",
+  },
   server: {
     port: 8080,
     open: true,
